37e1f8382c9a06abfa4e41ceb19bef09821d213f
chavo1 <42143239+chavo1@users.noreply.github.com>
2018-10-31 08:55:02 +0200

    Update plugin.sh

--- scripts/plugin.sh
+++ scripts/plugin.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 
+# Login as vagrant user
 su - vagrant
 
+# Compile a custom plugin
+echo "Compiling custom plugin..."
 go get github.com/petems/terraform-provider-extip
 
 cd ~/go/src/github.com/petems/terraform-provider-extip
 
 make build
 
+# Copy the custom plugin to the folder where is our main.tf
+echo "Copying compiled custom plugin to the required path"
 mkdir -p /vagrant/terraform.d/plugins/linux_amd64
 
 cp ~/go/bin/terraform-provider-extip /vagrant/terraform.d/plugins/linux_amd64/
 
+# Changing directory - where is our main.tf
 cd /vagrant
+
+echo "Initialize and apply terraform"
+terraform fmt
 
 terraform init
 
